15bc73fa13d731f69c4cae370adee1750c196e8c
余艳辉 <913918146@qq.com>
2019-01-11 14:33:59 +0800

    1，修复http的bug。

--- adas/src/main/java/com/ke/adas/CheckUpdateService.kt
+++ adas/src/main/java/com/ke/adas/CheckUpdateService.kt
@@ -3,10 +3,12 @@ package com.ke.adas
 import com.ke.adas.entity.CheckUpdateResult
 import io.reactivex.Observable
 import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
 interface CheckUpdateService {
 
+    @FormUrlEncoded
     @POST("Vispect_service_test/checkhasupdate")
     fun checkUpdate(
         /**
