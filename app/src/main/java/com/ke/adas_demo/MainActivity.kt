eefb9104b802115357427be1a32555e19bbcfaab
余艳辉 <913918146@qq.com>
2019-03-19 16:09:05 +0800

    1，升级jar。

--- app/src/main/java/com/ke/adas_demo/MainActivity.kt
+++ app/src/main/java/com/ke/adas_demo/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.ke.adas_demo
 
+import android.Manifest
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.tbruyelle.rxpermissions2.RxPermissions
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +13,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        video_test.setOnClickListener {
+            RxPermissions(this)
+                .request(Manifest.permission.ACCESS_COARSE_LOCATION)
+                .subscribe {
+                    if (it) {
+                        startActivity(Intent(this, VideoTestActivity::class.java))
+                    }
+                }
+        }
 
 
     }
---
+++ app/src/main/java/com/ke/adas_demo/VideoTestActivity.kt
@@ -0,0 +1,11 @@
+package com.ke.adas_demo
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * 测试同时调用三个获取视频的接口
+ */
+class VideoTestActivity : AppCompatActivity() {
+
+
+}
